6c68c914bd855cd46f091131882555e7bfb615ac
Vedantaz <muleyvedant4@gmail.com>
2024-09-04 20:36:01 +0530

    added remaining abstraction codes

---
+++ infosys/studentAbstraction.java
@@ -0,0 +1,43 @@
+// see at infosys springBoard
+class Student {
+    // Implement your code here
+}
+
+class UndergraduateStudent {
+    // Implment your code here
+}
+
+class GraduateStudent {
+    // Implment your code here
+}
+
+public class studentAbstraction {
+
+    public static void main(String[] args) {
+        UndergraduateStudent undergraduateStudent = new UndergraduateStudent("Philip");
+        undergraduateStudent.setTestScore(0, 70);
+        undergraduateStudent.setTestScore(1, 69);
+        undergraduateStudent.setTestScore(2, 71);
+        undergraduateStudent.setTestScore(3, 55);
+
+        undergraduateStudent.generateResult();
+
+        System.out.println("Student name: " + undergraduateStudent.getStudentName());
+        System.out.println("Result: " + undergraduateStudent.getTestResult());
+
+        System.out.println();
+
+        GraduateStudent graduateStudent = new GraduateStudent("Jerry");
+        graduateStudent.setTestScore(0, 70);
+        graduateStudent.setTestScore(1, 69);
+        graduateStudent.setTestScore(2, 71);
+        graduateStudent.setTestScore(3, 55);
+
+        graduateStudent.generateResult();
+
+        System.out.println("Student name: " + graduateStudent.getStudentName());
+        System.out.println("Result : " + graduateStudent.getTestResult());
+
+        // Create more objects of the classes for testing your code
+    }
+}
